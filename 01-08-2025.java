bcc410e0f52c4b721a384e9df5bbd23cb28a1108
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2025-08-01 19:05:20 +0530

    01-08-2025.java
    JAVA ARRAY PROGRAMS :
    
    1.Remove duplicate elements in an array.
    
    2.Move 0's to the end.

--- 01-08-2025.java
+++ 01-08-2025.java
@@ -1,4 +1,4 @@
-// remove duplicates
+1.// remove duplicates
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
@@ -36,6 +36,43 @@ public class Main{
          }
     }
 }
+
+//Output
+5
+1 2 3 2 1
+1 2 3 
+
+
+
+
+2.// 0 gets printed at the end
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        int b[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] != 0) {
+                b[index++] = a[i];
+            }
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
+}
+
+//Output
+5
+2 5 0 0 4
+2 5 4 0 0 
+
 
         
         
